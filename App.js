9bd1e3d014b70a82e48834c97bbbe94147885403
wisnuarizky <wisnuarizky01@gmail.com>
2024-06-18 19:28:25 +0700

    trying to make react navigation

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Pergi ke Progate"
+        onPress={() =>
+          navigation.navigate('Progate', {
+            name: 'Wisnu Arizky',
+            language: 'React Native',
+          })
+        }
+      />
+    </View>
+  );
+};
+
+const ProgateScreen = ({ navigation, route }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Progate!, {route.params.name}</Text>
+      <Text>Ayo belajar {route.params.language}!</Text>
+      <Button title="Kembali" onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Progate" component={ProgateScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
